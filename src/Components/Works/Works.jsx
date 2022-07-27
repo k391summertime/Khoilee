5970ce197940449e88a3dc8167f9b2e7e1c09984
phuoc <lehuyphuoc0105@gmail.com>
2022-07-28 00:57:04 +0700

    updateCode

---
+++ src/Components/Works/Works.jsx
@@ -0,0 +1,65 @@
+import React from "react";  
+import './Works.css';
+import Upwork from "../../img/Upwork.png";
+import Fiverr from "../../img/fiverr.png";
+import Amazon from "../../img/amazon.png";
+import Shopify from "../../img/Shopify.png";
+import Facebook from "../../img/Facebook.png";
+import {themeContext} from "../../Context";
+import { useContext } from "react";
+import {motion} from 'framer-motion';
+
+
+const Works = ()=>{
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+    return(
+  <div className="works">
+        <div className="awesome">
+            <span style={{color: darkMode? 'white' :''}}>Works for All these</span>
+            <span>Brancds & Clients</span>
+            <spane>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <br/>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 
+            <br/>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            <br/>
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+            </spane>
+            <button className="button s-button">Hire me</button>
+            <div className="blur s-blur1" style={{background: "#ABF1FF94"}}></div>
+        </div>
+        <div className="w-right">
+          <motion.div 
+          initial = {{rotate: 60}}
+          whileInView = {{rotate: 0}}
+          viewport = {{margin: '-40px'}}
+          transition = {{duration: 3.5, type: 'spring'}}
+
+          
+          className="w-mainCircle">
+            <div className="w-seeCircle">
+              <img src={Upwork} alt={''}/>
+            </div>
+            <div className="w-seeCircle">
+              <img src={Fiverr} alt={''}/>
+            </div>
+            <div className="w-seeCircle">
+              <img src={Amazon} alt={''}/>
+            </div>
+            <div className="w-seeCircle">
+              <img src={Shopify} alt={''}/>
+            </div>
+            <div className="w-seeCircle">
+              <img src={Facebook} alt={''}/>
+            </div>
+          </motion.div> 
+          <div className="w-backCircle blueCircle"></div>
+          <div className="w-backCircle yellowCircle"></div>
+
+        </div>
+  </div>
+    )
+}
+export default Works
